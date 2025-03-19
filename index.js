c12437b2dc390bacc1206ee63f00055e3719737b
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-03-19 10:22:05 +0300

    Config: enable cors

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import connectDB from './config/db.js';
 import taskRoutes from './routes/taskRoutes.js'
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors())
+
 
 const PORT = process.env.PORT;
 
 connectDB();
 
-app.get("/", (req, res) => res.send("Task API is running"));
-app.use("/api/tasks", taskRoutes);
+app.get("/", cors, (req, res) => res.send("Task API is running"));
+app.use("/api/tasks", cors, taskRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 }) 
